60a4cf7ab3cea28a64d8ef2811f94d6b48d7e387
Dmitry Nuzhin <iruirc@gmail.com>
2024-07-11 18:36:10 +0300

    Change dependency to SwiftyGif, SDWebImage from branch VSDC to version exact versions

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
             targets: ["DKPhotoGallery"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/iruirc/SwiftyGif.git", branch: "VSDC"),
-        .package(url: "https://github.com/iruirc/SDWebImage.git", branch: "VSDC"),
+        .package(url: "https://github.com/iruirc/SwiftyGif.git", exact: "5.4.500"),
+        .package(url: "https://github.com/iruirc/SDWebImage.git", exact: "15.19.100"),
     ],
     targets: [
         .target(
